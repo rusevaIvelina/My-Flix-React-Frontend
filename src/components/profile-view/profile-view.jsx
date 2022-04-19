ac311c79af42de0c7405d05d84a77043f41c54dd
rusevaIvelina <iva.ruseva14@gmail.com>
2022-04-18 22:00:16 -0400

    removed extra import

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import axios from "axios";
-//import { Link } from "react-router-dom";
 import "./profile-view.scss";
 
 export class ProfileView extends React.Component {
